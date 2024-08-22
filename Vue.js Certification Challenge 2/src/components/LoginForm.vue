c9aef27507c7613575a2b5c8acc4ef8c056f9db8
yordan.ramchev <69005114+Yordan-Ramchev@users.noreply.github.com>
2024-08-22 14:50:53 +0300

    feat: complete task 2

--- Vue.js Certification Challenge 2/src/components/LoginForm.vue
+++ Vue.js Certification Challenge 2/src/components/LoginForm.vue
@@ -1,17 +1,36 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, watch } from 'vue'
+const props = defineProps(['modelValue'])
+const emit = defineEmits(['update:modelValue'])
+const name = ref('')
+const password = ref('')
+
+watch(props, (newValue) => {
+    name.value = newValue.modelValue.username
+    password.value = newValue.modelValue.password
+  },
+{ immediate: true, deep: true}
+)
+
+function submit() {
+  const modelValue = { username: name.value , password: password.value}
+  emit('update:modelValue', modelValue)
+}
+</script>
+
 <template>
-  <form>
+  <form @submit.prevent="submit">
     <h1>Login</h1>
     <label>
       <span>Username</span>
-      <input type="text" />
+      <input type="text" v-model="name" />
     </label>
 
     <label>
       <span>Password</span>
-      <input type="password" />
+      <input type="password" v-model="password"  />
     </label>
 
-    <button>Login</button>
+    <button type="submit">Login</button>
   </form>
 </template>
