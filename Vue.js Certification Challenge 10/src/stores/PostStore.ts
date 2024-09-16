ce8bef8bb5c4334775b560fed57e05abfb018a31
yordan.ramchev <me@yramchev.com>
2024-09-16 17:35:44 +0300

    chore: complete task 10

--- Vue.js Certification Challenge 10/src/stores/PostStore.ts
+++ Vue.js Certification Challenge 10/src/stores/PostStore.ts
@@ -6,11 +6,48 @@ import { useCachedFetch } from "@/composables/useCachedFetch";
 export const usePostStore = defineStore("PostStore", () => {
   const posts = ref<Post[]>();
   const loadingList = ref(false);
-  function fetchPostList() {}
+  async function fetchPostList() {
+    loadingList.value = true;
+    const url = "/api/posts?fields=id,title,previewSnippet";
+
+    try {
+      const response = await fetch(url);
+
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+
+      posts.value = await response.json();
+    } catch (error) {
+      console.error(error?.message);
+    } finally {
+      loadingList.value = false;
+    }
+  }
 
   const loadingSingle = ref(false);
   const post = ref<Post>();
-  function fetchPostSingle() {}
+  async function fetchPostSingle({ id }) {
+    loadingSingle.value = true;
+
+    post.value = posts.value?.find((post) => post.id === id);
+
+    const url = `/api/posts/${id}`;
+
+    try {
+      const response = await fetch(url);
+
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+
+      post.value = await response.json();
+    } catch (error) {
+      console.error(error?.message);
+    } finally {
+      loadingSingle.value = false;
+    }
+  }
 
   return {
     // list of posts
