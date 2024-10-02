d9f30b720ba7ac4ff4ef6d71c7135626aec5a912
yordan.ramchev <me@yramchev.com>
2024-10-02 22:11:36 +0300

    chore: init task 11

---
+++ Vue.js Certification Challenge 11/tests/ToggleInput.spec.ts
@@ -0,0 +1,11 @@
+import { describe, it, expect, beforeEach } from "vitest";
+import { mount } from "@vue/test-utils";
+import ToggleInput from "../src/components/ToggleInput.vue";
+
+describe("the toggle input component", () => {
+  it.todo(
+    "toggles the v-model value between true and false when clicked",
+    async () => {},
+  );
+  it.todo("supports a binary v-model modifier", async () => {});
+});
