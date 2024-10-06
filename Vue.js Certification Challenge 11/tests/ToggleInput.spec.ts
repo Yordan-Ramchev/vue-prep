f7a7cfbf0b91d66fdbfa433e5a3eb2293d02c42f
yordan.ramchev <me@yramchev.com>
2024-10-06 13:37:31 +0300

    feat: complete task 11

--- Vue.js Certification Challenge 11/tests/ToggleInput.spec.ts
+++ Vue.js Certification Challenge 11/tests/ToggleInput.spec.ts
@@ -1,11 +1,54 @@
-import { describe, it, expect, beforeEach } from "vitest";
+import { describe, it, expect, beforeEach, test } from "vitest";
 import { mount } from "@vue/test-utils";
 import ToggleInput from "../src/components/ToggleInput.vue";
 
 describe("the toggle input component", () => {
-  it.todo(
-    "toggles the v-model value between true and false when clicked",
-    async () => {},
-  );
-  it.todo("supports a binary v-model modifier", async () => {});
+  test("toggles the v-model value between true and false when clicked", async () => {
+    const toggleInputComponent = mount(ToggleInput, {
+      attachTo: document.body,
+      props: {
+        modelValue: false,
+      },
+    });
+
+    // Click the component twice
+    await toggleInputComponent.trigger("click");
+    await toggleInputComponent.trigger("click");
+
+    // Get the values that were emitted
+    const emittedValues = toggleInputComponent.emitted("update:modelValue");
+
+    const valueOnFirstClick = emittedValues ? emittedValues[0][0] : null;
+    const valueOnSecondClick = emittedValues ? emittedValues[1][0] : null;
+
+    // On the first click the value should be true since we passed false for the initial prop value
+    expect(valueOnFirstClick).toBe(true);
+    // On the second click the value should be false since we clicked it again
+    expect(valueOnSecondClick).toBe(false);
+  });
+
+  test("supports a binary v-model modifier", async () => {
+    const toggleInputComponent = mount(ToggleInput, {
+      attachTo: document.body,
+      props: {
+        modelValue: false,
+        modelModifiers: { binary: true },
+      },
+    });
+
+    // Click the component twice
+    await toggleInputComponent.trigger("click");
+    await toggleInputComponent.trigger("click");
+    await toggleInputComponent.trigger("click");
+
+    // Get the values that were emitted
+    const emittedValues = toggleInputComponent.emitted("update:modelValue");
+    const valueOnFirstClick = emittedValues ? emittedValues[0][0] : null;
+    const valueOnSecondClick = emittedValues ? emittedValues[1][0] : null;
+
+    // On the first click the value should be true since we passed false for the initial prop value
+    expect(valueOnFirstClick).toBe(1);
+    // On the second click the value should be false since we clicked it again
+    expect(valueOnSecondClick).toBe(0);
+  });
 });
