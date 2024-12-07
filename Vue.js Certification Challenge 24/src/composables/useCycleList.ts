8f909dc31d058f5a5aa15ba3f7aa43261170d17d
yordan.ramchev <me@yramchev.com>
2024-12-07 13:52:00 +0200

    feat: complete task 22

--- Vue.js Certification Challenge 24/src/composables/useCycleList.ts
+++ Vue.js Certification Challenge 24/src/composables/useCycleList.ts
@@ -1,7 +1,37 @@
-export const useCycleList = () => {
+import { computed, ref, toRef } from "vue";
+
+export const useCycleList = (data: string[]) => {
+  const list = ref(data);
+  const index = ref(0);
+
+  const state = computed({
+    get() {
+      return list.value[index.value];
+    },
+    set(value) {
+      return (list.value[index.value] = value);
+    },
+  });
+
+  function prev() {
+    if (index.value === 0) {
+      index.value = data.length - 1;
+    } else {
+      index.value -= 1;
+    }
+  }
+
+  function next() {
+    if (index.value === data.length - 1) {
+      index.value = 0;
+    } else {
+      index.value += 1;
+    }
+  }
+
   return {
-    prev: () => {},
-    next: () => {},
-    state: "A single item in the array",
+    prev,
+    next,
+    state,
   };
 };
